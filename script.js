25217c9dc9ee063f9779f5439a9d595882de4512
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-05-03 07:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-//your JS code here. If required.
-let timeTakenP1 = 0, timeTakenP2 = 0, timeTakenP3 = 0;
-function promise1() {
-	const startTime = performance.now();
-	return new Promise((resolve, reject) => {
-		setTimeout(() => resolve('1'), getRandomIntInclusive(1, 3) * 1000)
-	}).then((value) => {
-	const endTime = performance.now();
-	timeTakenP1 = endTime - startTime;
-	})
+const res = document.getElementById("output");
+ 
+const promises = [
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
+  }),
+];
+ 
+async function callFns() {
+  const start = new Date();
+  // Use Promise.all to wait for all Promises to resolve
+  res.innerHTML += `
+            <tr id="loading">
+                <td colspan=2>Loading...</td>
+            </tr>
+          `;
+  await Promise.all(promises)
+    .then((results) => {
+      res.innerHTML = ``;
+      // Log the array of results
+      results.forEach((e) => {
+        res.innerHTML += `
+            <tr>
+                <td>${e.name}</td>
+                <td>${e.time}</td>
+            </tr>
+          `;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+ 
+  const end = new Date();
+ 
+  const timeInMillis = end - start;
+  res.innerHTML += `
+            <tr>
+                <td>Total</td>
+                <td>${timeInMillis / 1000}</td>
+            </tr>
+          `;
 }
-
-function promise2() {
-	const startTime = performance.now();
-	return new Promise((resolve, reject) => {
-		setTimeout(() => resolve('2'), getRandomIntInclusive(1, 3) * 1000)
-	}).then((value) => {
-	const endTime = performance.now();
-	timeTakenP2 = endTime - startTime;
-	})
-}
-
-function promise3() {
-	const startTime = performance.now();
-	return new Promise((resolve, reject) => {
-		setTimeout(() => resolve('3'), getRandomIntInclusive(1, 3) * 1000)
-	}).then((value) => {
-	const endTime = performance.now();
-	timeTakenP3 = endTime - startTime;
-	})
-}
-
-Promise.all([promise1(), promise2(), promise3()]).then(() => {
-	document.getElementById('pv-1').innerText = timeTakenP1 / 1000;
-	document.getElementById('pv-2').innerText = timeTakenP2 / 1000;
-	document.getElementById('pv-3').innerText = timeTakenP3 / 1000;
-	document.getElementById('total').innerText = (timeTakenP1 + timeTakenP2 + timeTakenP3) / 1000;
-})
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-}
+ 
+callFns();
